7e71c08b7df1e8db330201504ce4edfa32a2806e
Beckk12345 <litovbeks@gmail.com>
2022-07-16 20:41:48 +0300

    Initial commit

---
+++ src/ExAmple/User.java
@@ -0,0 +1,19 @@
+package ExAmple;
+
+public class User extends Object {
+    protected String name;
+    protected int age;
+
+    public User(String  name, int age){
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+
+        return "Hello: " + name + ". Your age: " + age ;
+    }
+}
+
+
---
+++ src/com/potok/Main.java
@@ -0,0 +1,21 @@
+package com.potok;
+
+
+import ExAmple.User;
+
+public class Main {
+       public static void main(String[] args) {
+
+           User user = new User("Ailin", 30);
+           System.out.println(user.toString());
+
+           User user1 = new User("Bek", 28);
+           System.out.println(user1.toString());
+
+
+       }
+
+        }
+
+
+
